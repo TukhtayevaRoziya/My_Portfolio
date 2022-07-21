b30593abfbb7503308be899a073634a75c033022
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-07-21 19:55:02 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 
-import Footer from "./components/Footer/Footer"
-import Header from "./components/Header/Header"
-import Resume from "./components/Resume/Resume"
-import Skills from "./components/Skills/Skills"
-import Switch from "./components/Switch/Switch"
-import Project from "./components/Project/Project"
-import Language from "./components/Language/Language"
+import Footer from "./components/footer/Footer"
+import Header from "./components/header/Header"
+import Resume from "./components/resume/Resume"
+import Skills from "./components/skills/Skills"
+import Switch from "./components/switch/Switch"
+import Project from "./components/project/Project"
+import Language from "./components/language/Language"
 
 import "./App.css"
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals'
 import { HashRouter } from 'react-router-dom'
->>>>>>> 0cf6fd1575611366c3476634340333e9bd730cd3
 
 import store from "./redux/store";
 import App from "./App";
