862de891e0a88c110733ec50c6986e3e18e2ebf4
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-07-21 21:06:02 +0500

    add preloader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Route, Routes } from "react-router-dom"
+import React, { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Footer from "./components/footer/Footer"
-import Header from "./components/header/Header"
-import Resume from "./components/resume/Resume"
-import Skills from "./components/skills/Skills"
-import Switch from "./components/switch/Switch"
-import Project from "./components/project/Project"
-import Language from "./components/language/Language"
+import Footer from "./components/footer/Footer";
+import Switch from "./components/switch/Switch";
+import Language from "./components/language/Language";
 
-import "./App.css"
+import "./App.css";
+import { Preloader } from "./components/preloader/Preloader";
+
+const Header = lazy(() => import("./components/header/Header"));
+const Resume = lazy(() => import("./components/resume/Resume"));
+const Skills = lazy(() => import("./components/skills/Skills"));
+const Project = lazy(() => import("./components/project/Project"));
 
 const App = () => {
   return (
@@ -19,10 +22,38 @@ const App = () => {
       </h2>
 
       <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/project" element={<Project />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Header />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/resume"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Resume />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/skills"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Skills />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/project"
+          element={
+            <Suspense fallback={<Preloader />}>
+              <Project />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </div>
